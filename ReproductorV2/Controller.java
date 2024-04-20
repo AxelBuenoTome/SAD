1fddd445721d2dac2fa7ce2ea2744679e7eeb755
Pepus Pella <josep.pella@estudiantat.upc.edu>
2024-04-20 14:04:00 +0200

    actualización desde VSC de Josep

--- ReproductorV2/Controller.java
+++ ReproductorV2/Controller.java
@@ -1,5 +1,82 @@
 package ReproductorV2;
 
-public class Controller {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Controller extends BufferedReader {
+
+    public Controller(InputStreamReader in) {
+        super(in);
+    }
+
+    public void setRaw() {
+        String[] cmd = { "/bin/sh", "-c", "stty -echo raw </dev/tty" };
+        try {
+            Runtime.getRuntime().exec(cmd);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void unsetRaw() {
+        String[] cmd = { "/bin/sh", "-c", "stty echo cooked </dev/tty" };
+        try {
+            Runtime.getRuntime().exec(cmd);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+  @Override
+  public int read() throws IOException {
+      int inputChar;
+      if (match(KEY.UP_STR)){
+          return KEY.UP_VAL;
+      }
+      if (match(KEY.DOWN_STR)){
+          return KEY.DOWN_VAL;
+      }
+      inputChar = super.read();
+      return inputChar; 
+  }
+
+  private boolean match(String escape) throws IOException{
+      //lo primero es guardar el estado actual del lector con mark(), perteneciente a BufferedReader
+      //indicamos el tamaño del String escape, para luego volver atrás la misma cifra
+      mark(escape.length());
+      try{
+          for(int i=0 ; i<escape.length(); i++){
+              int ch = super.read();
+              //miramos si el caracter leído coincide con el String escape, caracter a caracter.
+              if(ch != escape.charAt(i)){
+                  //si no hay match usamos el reset y devolvemos false (para el backtracking)
+                  reset();
+                  return false;
+              }
+          }
+          return true;
+      }finally{
+      }
+
+  }
+
+ public String readLine() throws IOException {
+     setRaw();
+     int actualChar;
+     while ((actualChar = this.read()) != KEY.ENTER) {
+         switch (actualChar) {
+
+             case KEY.UP_VAL:
+                 Model.rightArrow();
+                 break;
+
+             case KEY.DOWN_VAL:
+                 Model.leftArrow();
+                 break;
+
+         }
+     }
+     unsetRaw();
+ }
 }
---
+++ ReproductorV2/KEY.java
@@ -0,0 +1,24 @@
+package ReproductorV2;
+
+public class KEY {
+    //Clase EY de la practica 1 simplificada para leer secuencias de escape
+    public static final int ENTER = 13; // ENTER es el 13
+    public static final int SQUARE_BRACKET = 91; // "["
+    public static final int TILDE = 126; // "~"
+    public static final int BSK = 127; // backspace "\b"
+
+    // Reasignaciones de la tabla ASCII, a partir de valores que no usamos
+    public static final int DEL_VAL = -1;
+    public static final int UP_VAL = -2;
+    public static final int DOWN_VAL = -3;
+    public static final int RIGHT_VAL = -4;
+    public static final int LEFT_VAL = -5;
+
+    //Strings Código ANSI
+    public static final String DEL_STR = "\033[3~";
+    public static final String UP_STR = "\033[A";
+    public static final String DOWN_STR = "\033[B";
+    public static final String RIGHT_STR = "\033[C";
+    public static final String LEFT_STR = "\033[D";
+}
+
